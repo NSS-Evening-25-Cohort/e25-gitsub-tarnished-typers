7db2c17cf4b85060a1686944fac62a4b81f6950e
braydenbubp <braydenbubp1@gmail.com>
2023-11-07 20:17:35 -0500

    initializing packages

---
+++ packages.js
@@ -0,0 +1,21 @@
+const packages = [
+  {
+    id: 1,
+    name: "Docker",
+    description: "software platform for building apps based on containers",
+  },
+
+  {
+    id: 1,
+    name: "Apache Maven",
+    description: "default package manager used for javascript",
+  },
+
+  {
+    id: 1,
+    name: "NuGet",
+    description: "open source package manager used for microsoft dev platforms",
+  },
+]
+
+
