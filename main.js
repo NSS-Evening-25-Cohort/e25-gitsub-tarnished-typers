db32103b7d8b7bfdab4fe61ede9d1fce9756dfc4
glassoctopus <glassoctopus@yahoo.com>
2023-11-10 23:53:24 -0600

    overview working, needs more

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ btnOverView.innerHTML = `<img src="images/OverviewIcon.png" alt="overview Icon" 
 btnOverView.id = "overview";
 btnOverView.className = "btn btn-outline-light";
 btnOverView.type = "button";
-btnOverView.onclick = function(){alert("Dynamic Overview button Clicked")};
+// btnOverView.onclick = function(){alert("Dynamic Overview button Clicked")};
 
 btnRepostories.innerHTML=`<img src="images/RepositoriesIcon.png" alt="repositories Icon" width="25" height="25">Repositories`;
 btnRepostories.id = "repositories";
 btnRepostories.className = "btn btn-outline-light";
 btnRepostories.type = "button";
-btnRepostories.onclick = function(){alert("Dynamic Repositories button Clicked")};
+// btnRepostories.onclick = function(){alert("Dynamic Repositories button Clicked")};
 
 btnProjects.innerHTML=`<img src="images/ProjectsIcon.png" alt="projects Icon" width="25" height="25">Projects`;
 btnProjects.id = "projects";
@@ -31,7 +31,7 @@ btnPackages.innerHTML=`<img src="images/PackagesIcon.png" alt="packages Icon" wi
 btnPackages.id = "packages";
 btnPackages.className = "btn btn-outline-light";
 btnPackages.type = "button";
-btnPackages.onclick = function(){alert("Dynamic Packages button Clicked")};
+// btnPackages.onclick = function(){alert("Dynamic Packages button Clicked")};
 
 navbuttons = [btnOverView, btnRepostories, btnProjects, btnPackages]
 
@@ -69,6 +69,32 @@ const renderToDom = (array) => {
   console.log(outputTop);
 }
 
+const renderOverview = () => {
+  console.log("did i render the correct thing?")
+  let topOutput = "";
+  outputTop.innerHTML = topOutput;
+  
+    topOutput = `
+    <div class="rcorners">
+      <h4>Ranni McColbupalenick</h4>
+      <h2>Hi, I'm Ranni McColbupalenick &#128075; &#128187;</h2>
+        <div id="overviewDiv">
+          <div class="inline">
+            <h1>Ranni McColbupalenick</h1>
+            <h4 >slayer of gods, software and spells, parry =()=> reposte</h4>          
+          </div>
+          <div class="inline">
+            <img src="images/https_zoo_replicate_dev head shot of a professional female computer scientist SDXL.png" alt="Ranni Pic" width="250" height="250">
+          </div>          
+        </div>
+      </div>
+    <hr>
+    `
+
+  outputTop.innerHTML = topOutput;
+  console.log(topOutput);
+}
+
 
 navBar.addEventListener("click", (event) => {
 
@@ -82,6 +108,10 @@ navBar.addEventListener("click", (event) => {
 } else if (event.target.id.includes("projects")){
   projectsToDom(projects)
   projectFormToDom()
-}  
+} else if (event.target.id.includes("overview")){
+  console.log("Overview?")
+  renderOverview();
+  
+} 
 
 });
